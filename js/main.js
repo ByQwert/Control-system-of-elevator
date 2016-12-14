67f451e55ab29fc743fbb6617e2b3092a5a68e8f
ByQwert <byqwerton@gmail.com>
2016-12-14 23:17:51 +0300

    Realize full moving complex
    + add and change some related lines in index.html ans style.css
    + disable some functions for working prototype

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $( "#generate-form" ).dialog({
 	modal: true,
 	buttons: {
 		"Generate": function() {
-			if ($('input[name="floors"]').val() > 0 && $('input[name="humans"]').val() > 0) {
+			if ($('input[name="floors"]').val() > 0 && $('input[name="humans"]').val() > -1) {
 				mainFrame.generate(+$('input[name="floors"]').val(),+$('input[name="humans"]').val());
 				$( this ).dialog( "close" );				
 			} else {	
@@ -57,10 +57,6 @@ dialog = $( "#add-human-div" ).dialog({
    },
 });
 
-function launchSystem() {
-	elevator.chooseNextFloor();
-}
-
 // Classes
 function MainFrame() {
 	this.generate = function(floors,humans) {
@@ -87,16 +83,25 @@ function MainFrame() {
 		$("#amount-of-floors").text(house.amountOfFloors);
 		$("#amount-of-humans").text(house.amountOfHumans);		
 		for (var i = 0; i < house.amountOfHumans; i++) {
-			$("#pending").append('<h6>' + house.pending[i].name + '</h6><p>' + house.pending[i].state + house.pending[i].spawnFloor + '</p>');
+			$("#pending").append('<h6>' + house.pending[i].name + '</h6><p id=' + house.pending[i].ID + '>' + house.pending[i].state + '</p>');
 		}		
 		$("#main-table").show();	
 		$( "#pending" ).accordion({
  			collapsible: true,
  			active: false
  		});
+ 		$( "#passengers" ).accordion({
+ 			collapsible: true,
+ 			active: false
+ 		});
+ 		$( "#delivered" ).accordion({
+ 			collapsible: true,
+ 			active: false
+ 		});
 	};
 	this.launchSystem = function() {
-		
+		elevator.chooseNextFloor();
+		$("[onclick=mainFrame\\.launchSystem\\(\\)]").prop('disabled', true);
 	};
 	this.stopSystem = function() {
 
@@ -123,15 +128,18 @@ function MainFrame() {
 			// Update statistic
 			statistic.amountOfCreatedHumans++;
 			// GUI
-			$("#amount-of-humans").text(house.amountOfHumans);			
-			$("#pending").append('<h6>' + house.pending[house.pending.length-1].name + '</h6><p>' + house.pending[house.pending.length-1].state + house.pending[house.pending.length-1].spawnFloor + '</p>');
+			$("#amount-of-humans").text(house.pending.length+elevator.passengers.length);			
+			$("#pending").append('<h6>' + house.pending[house.pending.length-1].name + '</h6><p id='+ house.pending[house.pending.length-1].ID +'>' + house.pending[house.pending.length-1].state  + '</p>');
 			$( "#pending" ).accordion( "refresh" );
 			dialog.dialog("close");
 			$("#add-human-form")[0].reset();
 		} else {
 			alert("Invalid data!");
 			validator = true;
-		}		
+		}
+		if (!buttonsOfElevator.some(function(button) {return button.state == true })) {
+			elevator.chooseNextFloor();
+		}
 	}
 }
 
@@ -139,6 +147,7 @@ function House(floors,humans) {
 	this.amountOfFloors = floors;
 	this.amountOfHumans = humans;
 	this.pending = [];
+	this.delivered = [];
 }
 
 function Elevator() {
@@ -151,55 +160,184 @@ function Elevator() {
 	this.passengers = [];
 	this.priorityDistance = house.amountOfFloors-1;
 	this.chooseNextFloor = function() {
-		if (this.passengers.length == 0) {
-			for (var i = 0; i < buttonsOfHouse.length; i++) {
-				if (buttonsOfHouse[i].state) {
-					if (this.currentFloor-1 == i) {
+		if (buttonsOfElevator.some(function(button) {return button.state == true })) {
+			for (var i = 0; i < buttonsOfElevator.length; i++) {
+				if (buttonsOfElevator[i].state) {
+					if (Math.abs(this.currentFloor-i) < this.priorityDistance) {
+						this.priorityDistance = Math.abs(this.currentFloor-i);
 						this.targetFloor = i+1;
-						break;
-					} else {
-						if (Math.abs(this.currentFloor-i) < this.priorityDistance) {
-							this.priorityDistance = Math.abs(this.currentFloor - i);
-							this.targetFloor = i+1;
-						}
-					}					
+					}
 				}
 			}
+			console.log(this.targetFloor);	
+			this.priorityDistance = house.amountOfFloors-1;	
 			this.move();
-		}		
-	};
-	this.wait = function() {
+		} else {
+			if (buttonsOfHouse.some(function(button) {return button.state == true })) {
+				for (var i = 0; i < buttonsOfHouse.length; i++) {
+					if (buttonsOfHouse[i].state) {
+						if (this.currentFloor-1 == i) {
+							this.targetFloor = i+1;
+							break;
+						} else {
+							if (Math.abs(this.currentFloor-i) < this.priorityDistance) {
+								this.priorityDistance = Math.abs(this.currentFloor - i);
+								this.targetFloor = i+1;
+							}
+						}					
+					}
+				}
+				console.log(this.targetFloor);	
+				this.priorityDistance = house.amountOfFloors-1;	
+				this.move();
+			}
+		}	
 	};
 	this.move = async function() {
+		if (this.currentFloor == this.targetFloor) {
+			this.doorState = true;
+			this.state = "Staying with opened doors";	
+			$("#animation").html(this.state + "<br>" + this.currentFloor);
+			this.wait();
+		}
+		// Moving up
 		if (this.currentFloor < this.targetFloor) {
 			this.state = "Moving up";		
 			$("#animation").html(this.state + "<br>" + this.currentFloor);
-			//console.log("moving");
+			if (this.passengers.length > 0) {
+				this.passengers.forEach(function(human) {
+					human.state = "Moving up at the level of the floor " +  elevator.currentFloor;
+					// GUI
+					$("#passengers").children('#' + human.ID).text(human.state);
+				});
+			}
 			await sleep(settings.speed);
 			for (var i = this.currentFloor+1; i != this.targetFloor+1; i++) {	
 				this.currentFloor = i;
 				if (i != this.targetFloor) {
 					$("#animation").html(this.state + "<br>" + this.currentFloor);
+					if (this.passengers.length > 0) {
+						this.passengers.forEach(function(human) {
+							human.state = "Moving up at the level of the floor " +  elevator.currentFloor;
+							// GUI
+							$("#passengers").children('#' + human.ID).text(human.state);
+						});
+					}
 					//console.log(this.currentFloor);
 					//console.log("moving");
 					await sleep(settings.speed);
 				} else {
 					this.doorState = true;
 					this.state = "Staying with opened doors";	
+					// Animation
 					$("#animation").html(this.state + "<br>" + this.currentFloor);
-					//console.log("target");
+					// GUI
+					this.passengers.forEach(function(human) {
+						human.state = "Moving up at the level of the floor " +  elevator.currentFloor;
+						// GUI
+						$("#passengers").children('#' + human.ID).text(human.state);
+					});				
+					this.wait();
 				}
 			}
 		} 
+		// Moving down
 		if (this.currentFloor > this.targetFloor) {
-
-		}
-		if (this.currentFloor == this.targetFloor) {
-			this.doorState = true;
-			this.state = "Staying with opened doors";	
+			this.state = "Moving down";
 			$("#animation").html(this.state + "<br>" + this.currentFloor);
+			if (this.passengers.length > 0) {
+				this.passengers.forEach(function(human) {
+					human.state = "Moving down at the level of the floor " +  elevator.currentFloor;
+					// GUI
+					$("#passengers").children('#' + human.ID).text(human.state);
+				});
+			}
+			await sleep(settings.speed);
+			for (var i = this.currentFloor-1; i != this.targetFloor-1; i--) {
+				this.currentFloor = i;
+				if (i != this.targetFloor) {
+					$("#animation").html(this.state + "<br>" + this.currentFloor);	
+					if (this.passengers.length > 0) {
+						this.passengers.forEach(function(human) {
+							human.state = "Moving down at the level of the floor " +  elevator.currentFloor;
+							// GUI
+							$("#passengers").children('#' + human.ID).text(human.state);
+						});	
+					}		
+					await sleep(settings.speed);
+				}	else {
+					this.doorState = true;
+					this.state = "Staying with opened doors";
+					// Animation
+					$("#animation").html(this.state + "<br>" + this.currentFloor);
+					// GUI
+					this.passengers.forEach(function(human) {
+						human.state = "Moving down at the level of the floor " +  elevator.currentFloor;
+						// GUI
+						$("#passengers").children('#' + human.ID).text(human.state);
+					});				
+					this.wait();
+				}
+			}
 		}
-		//this.passengers.push()
+	};
+	this.wait =  async function() {		
+		await sleep(1000);
+		// Exit
+		j = 0;
+		if (this.passengers.length) {
+			this.passengers.forEach(function(human,i) {			
+				if (human.targetFloor == elevator.currentFloor) {
+					human.state = "Delivered to the target floor " + human.targetFloor;
+					house.delivered.push(human);
+					delete elevator.passengers[i];
+					buttonsOfElevator[human.targetFloor-1].state = false;
+					// GUI
+					$("#delivered").append('<h6>' + house.delivered[j].name + '</h6><p id=' + house.delivered[j].ID + '>' + house.delivered[j].state + '</p>');
+					$( "#delivered" ).accordion( "refresh" );
+					$("#passengers").children('[aria-controls=' + human.ID + ']').remove();
+					$("#passengers").children('#' + human.ID).remove();
+					$( "#passengers" ).accordion( "refresh" );
+					j++;
+				}
+			});
+			this.passengers = this.passengers.filter(function(human) {
+				return human != "undefined";
+			});	
+			setTimeout(function() { 
+				house.amountOfHumans -= house.delivered.length;
+				house.delivered.splice(0);
+				// GUI
+				$("#delivered").empty();
+				$("#amount-of-humans").text(house.amountOfHumans);
+			}, 2000);
+		}			
+		// Entrance
+		j = this.passengers.length;
+		house.pending.forEach(function(human, i) {
+			//console.log(human.spawnFloor, elevator.currentFloor);
+			if (human.spawnFloor == elevator.currentFloor) {
+				human.state = "Staying in elevator on " + elevator.currentFloor + " floor";
+				elevator.passengers.push(human);
+				delete house.pending[i];
+				buttonsOfHouse[human.spawnFloor-1].state = false;
+				human.pressTargetFloorButton();
+				// GUI 
+				$("#passengers").append('<h6>' + elevator.passengers[j].name + '</h6><p id=' + elevator.passengers[j].ID + '>' + elevator.passengers[j].state + '</p>');
+				$( "#passengers" ).accordion( "refresh" );
+				$("#pending").children('[aria-controls=' + human.ID + ']').remove();
+				$("#pending").children('#' + human.ID).remove();
+				$( "#pending" ).accordion( "refresh" );
+				j++;
+			}
+		});
+		house.pending = house.pending.filter(function(human) {
+			return human != "undefined";
+		});
+		this.doorsState = false;
+		this.state = "Staying with closed doors";	
+		$("#animation").html(this.state + "<br>" + this.currentFloor);
+		this.chooseNextFloor();
 	};
 }
 
@@ -223,7 +361,8 @@ function Human(name, weight, spawnFloor, targetFloor) {
 	this.wait = function() {
 	}
 	this.pressSpawnFloorButton();
-	this.state = "Waiting for elevator on floor ";	
+	this.state = "Waiting for elevator on " + this.spawnFloor + " floor";	
+	this.ID = this.name + '-' + this.spawnFloor + '-' + this.targetFloor;
 }
 
 function Button(floor) {
